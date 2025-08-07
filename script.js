e6a329e878163c1f0505a226219dacc1483a8f3c
Favskid <favouroloda24@gmail.com>
2025-08-07 22:03:14 +0100

    adding new changes

--- script.js
+++ script.js
@@ -6,20 +6,13 @@ const submit = document.getElementById('btn');
 const items = document.querySelectorAll('.item');
 const selectedNumber = document.getElementById('selected-number');
 
-
 items.forEach(item => {
     item.addEventListener('click', () => {
-        items.forEach(itemz => {
-            itemz.style.backgroundColor = 'hsla(217, 12%, 63%, 0.3)';
-            itemz.style.color = 'hsl(0, 100%, 100%)';
-        })
-        item.style.backgroundColor = 'hsl(0, 100%, 100%)';
-        item.style.color = 'hsl(216, 12%, 8%)';
-
+        items.forEach(i => i.classList.remove('selected'));
+        item.classList.add('selected');
         selectedRatings = item.textContent;
-    });
-});
-
+    })
+})
 
 submit.addEventListener('click', () => {
     if (selectedRatings !== null ) {
